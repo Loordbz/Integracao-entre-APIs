10e03b701ac013fa1121377f798a9e19605d31b0
Leo <l300000000@gmail.com>
2023-05-10 16:11:16 -0300

    feat: Criação do Service

---
+++ Service/Service/EnderecoService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Domain.Interface.Service;
+using Domain.Models.DTO;
+using Domain.Models.ViewModels;
+
+namespace Service.Service;
+
+public class EnderecoService : IEnderecoService
+{
+    private readonly IBrasilApi _service;
+    private readonly IMapper _mapper;
+
+    public EnderecoService(IBrasilApi service, IMapper mapper)
+    {
+        _service = service;
+        _mapper = mapper;
+    }
+    
+    public async Task<ResponseGenerico<EnderecoViewModel>> GetByCep(string cep)
+    {
+        var endereco = await _service.GetByCep(cep);
+        return _mapper.Map<ResponseGenerico<EnderecoViewModel>>(endereco);
+    }
+}
