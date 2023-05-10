1fde38186e236a6e28ce3ab35be5796708bf57a6
Leo <l300000000@gmail.com>
2023-05-10 16:17:17 -0300

    feat: Criação do mapeamento do AutoMapper

---
+++ Infra/Data/Mappings/EnderecoMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Domain.Models;
+using Domain.Models.DTO;
+using Domain.Models.ViewModels;
+
+namespace Infra.Data.Mappings;
+
+public class EnderecoMapping : Profile
+{
+    public EnderecoMapping()
+    {
+        CreateMap(typeof(ResponseGenerico<>), typeof(ResponseGenerico<>));
+        CreateMap<EnderecoViewModel, Endereco>();
+        CreateMap<Endereco, EnderecoViewModel>();
+    }
+    
+}
