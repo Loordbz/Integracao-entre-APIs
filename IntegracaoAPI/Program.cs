b6a0e3df3607be9b4e17124c9e959b2500809fa7
Leo <l300000000@gmail.com>
2023-05-10 16:20:56 -0300

    feat: Configuração do builder

--- IntegracaoAPI/Program.cs
+++ IntegracaoAPI/Program.cs
@@ -1,3 +1,8 @@
+using Domain.Interface.Service;
+using Infra.Data.Mappings;
+using Infra.Repository;
+using Service.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IEnderecoService, EnderecoService>();
+builder.Services.AddSingleton<IBrasilApi, BrasilApiRepository>();
+
+builder.Services.AddAutoMapper(typeof(EnderecoMapping));
 
 var app = builder.Build();
 
