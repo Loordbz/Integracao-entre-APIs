d5be2352763ef54e6a3c5a49f96e0f025ddfb284
Leo <l300000000@gmail.com>
2023-05-10 15:58:09 -0300

    feat: Criação do models

---
+++ Domain/Models/Endereco.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Models;
+
+public class Endereco
+{
+    [JsonPropertyName("cep")]
+    public string? Cep { get; set; }
+
+    [JsonPropertyName("state")]
+    public string? Estado { get; set; }
+
+    [JsonPropertyName("city")]
+    public string? Cidade { get; set; }
+
+    [JsonPropertyName("neighborhood")]
+    public string? Bairro { get; set; }
+
+    [JsonPropertyName("street")]
+    public string? Rua { get; set; }
+
+    [JsonPropertyName("service")]
+    public string? Servico { get; set; }
+}
